c82caa8d67993f59d48c10a291d5e3208cd05bb2
kesonyk <keson@bupt.edu.cn>
2015-12-16 17:50:20 +0800

    fix the bug

--- centerServer.py
+++ centerServer.py
@@ -6,7 +6,7 @@ import sqlite3
 conn=sqlite3.connect('center.db')
 conn.text_factory=str
 carMap={}
-distMap={}
+distMap={'01':'300'}
 
 
 def isTabExist(tabName):
@@ -20,7 +20,6 @@ def isTabExist(tabName):
  RPC Server Operation
 """
 class CenterRPC(object):
-
 	def __init__(self):
 		if not isTabExist('PERMISSION'):
 		#create the Permission Tab
@@ -29,6 +28,13 @@ class CenterRPC(object):
 					  CARID  TEXT  NOT NULL,
 					  CARSRC   TEXT  NOT NULL)''')
 			print "Permission Table created successfully"
+			conn.execute("INSERT INTO PERMISSION (GATEID,CARID,CARSRC)\
+			VALUES ('01','001','fffffffffffffff1')")
+
+
+				
+
+	
 
 		if not isTabExist('DATABASE'):
 			conn.execute('''CREATE TABLE DATABASE
@@ -36,26 +42,24 @@ class CenterRPC(object):
 					  	CARID  TEXT  NOT NULL,
 					  	DATA   TEXT  NOT NULL,
 					  	STATE  TEXT  NOT NULL)''')
-			print "Table created successfully"
+			print "Database Table created successfully"
+			conn.execute("INSERT INTO DATABASE (GATEID,CARID,DATA,STATE)\
+			VALUES ('01','fffffffffffffff1','',0)")
 
+
+	
 		if not isTabExist('DISTTAB'):
 
 			conn.execute('''CREATE TABLE DISTTAB
 						( GATEID TEXT NOT NULL,
 						  DIST	 TEXT NOT NULL)''')
 
-			print "Table created successfully"
+			print "Distance Table created successfully"
 
-
-		conn.execute("INSERT INTO PERMISSION (GATEID,CARID,CARSRC)\
-			VALUES ('01','001','fffffffffffffff1')")
-
-
-		conn.execute("INSERT INTO DATABASE (GATEID,CARID,DATA,STATE)\
-			VALUES ('01','fffffffffffffff1','',0)")
-
-		conn.execute("INSERT INTO DISTTAB (GATEID,DIST)\
+			conn.execute("INSERT INTO DISTTAB (GATEID,DIST)\
 			VALUES ('01','200')")
+
+
 
 
 		cursor=conn.execute("SELECT * from PERMISSION")
@@ -68,8 +72,15 @@ class CenterRPC(object):
 		for row in cursor:
 			print type(row)
 			print row
+		
+		cursor=conn.execute("SELECT GATEID,DIST from DISTTAB")
+		for row in cursor:
+			print type(row)
+			print row
+		conn.commit()
 
 		conn.close()
+
 	def dataSelect(self,gateId):
 			
 		conn=sqlite3.connect('center.db')
@@ -89,7 +100,7 @@ class CenterRPC(object):
 		
 		conn=sqlite3.connect('center.db')
 		cursor=conn.execute("SELECT GATEID,CARSRC from PERMISSION")
-
+	
 		for row in cursor:
 			print carId,row
 			if gateId==row[0] and carSrc==row[1]:
@@ -110,14 +121,12 @@ class CenterRPC(object):
 		conn.close()
 
 	def distInfo(self):
-		
 		conn=sqlite3.connect('center.db')
-		distMap={}
 		cursor=conn.execute("SELECT GATEID,DIST from DISTTAB")
 		for row in cursor:
-			print row
 			distMap[row[0]]=row[1]
 		conn.close()
+		print distMap
 		return distMap;
 
 
@@ -125,17 +134,6 @@ if(__name__=='__main__'):
 	s=zerorpc.Server(CenterRPC())
 	s.bind("tcp://0.0.0.0:4242")
 	s.run()
-
-
-
-
-
-
-
-
-
-
-
 
 
 
