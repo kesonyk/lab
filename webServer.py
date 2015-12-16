7958f8c83f99a7af1d2bb8d6c36169ff4c53cff5
kesonyk <keson@bupt.edu.cn>
2015-12-16 20:42:17 +0800

    fix web

--- webServer.py
+++ webServer.py
@@ -118,12 +118,12 @@ def set_dist():
 		if not session.get('logged_in'):
 			abort(401)
 		db=get_db()
-		db.execute('UPDATE DISTTAB SET GATEID=?,DIST=?',
-					[request.form['gateid'],request.form['dist']])
+		db.execute('UPDATE DISTTAB SET DIST=? WHERE GATEID=?',
+					(request.form['dist'],request.form['gateid']))
 		db.commit()
 		flash('New distance has been seted ')
-		return redirect(url_for('show_legal'))
-	return render_template('setDistance.html')
+		return redirect(url_for('set_dist'))
+	return render_template('setDistance.html',entries=entries)
 
 
 @app.route('/add_permission',methods=['GET','POST'])
@@ -140,8 +140,8 @@ def add_permission():
 					[request.form['GATEID'],request.form['CARID'],request.form['CARSRC']])
 		db.commit()
 		flash('New entry was successfully posted')
-		return redirect(url_for('home'))
-	return render_template('add_permission.html')
+		return redirect(url_for('add_permission'))
+	return render_template('add_permission.html',entries=entries)
 
 
 if __name__=='__main__':
